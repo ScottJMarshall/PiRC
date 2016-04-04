d56751ab099c1d0b7370907afc2eb9880437f2b6
Scott Marshall <scott@sjmarshall.com>
2016-04-04 07:21:28 -0700

    Update test.js so that its only a canary check

--- test/test.js
+++ test/test.js
@@ -1,9 +1,7 @@
 var assert = require('assert');
-describe('Array', function() {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal(-1, [1,2,3].indexOf(5));
-            assert.equal(-1, [1,2,3].indexOf(0));
-        });
+var expect = require('chai').expect;
+describe("The Test Environment", function() {
+    it("checks that true is true", function() {
+        expect(true).to.be.true;
     });
 });
